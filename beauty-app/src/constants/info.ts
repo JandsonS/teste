8d9c6910e653268ba879889fd57e139f03143b03
Jose Jandson <jandsonramalho@gmail.com>
2026-01-14 14:03:50 -0300

    numeronovo

--- beauty-app/src/constants/info.ts
+++ beauty-app/src/constants/info.ts
@@ -2,7 +2,7 @@ export const SITE_CONFIG = {
   // Dados do Cliente (Só mexa aqui!)
   name: "Studio de Beleza",
   description: "Agende seu horário para Design de Sobrancelhas, Cílios e Micropigmentação.",
-  whatsappNumber: "5587991537080", // Seu número (com 55)
+  whatsappNumber: "5581989015555", // Seu número (com 55)
   email: "contato@studio.com.br",
   url: "https://teste-drab-rho-60.vercel.app", // Link do site
 
