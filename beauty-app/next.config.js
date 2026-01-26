4b5846a689b0b420e724db0f5959a1da25d51a43
Jose Jandson <jandsonramalho@gmail.com>
2026-01-26 10:28:19 -0300

    novaconfig

---
+++ beauty-app/next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
+const nextConfig = {
+  reactStrictMode: true,
+  // Se você tiver outras configs (como images), coloque aqui
+};
+
+module.exports = withPWA(nextConfig);   
